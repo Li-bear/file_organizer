7dd0588adb3d0fd1b34bbd154f970927ecbe3602
Li-bear <allisoncastellanos8@gmail.com>
2021-03-24 08:33:50 -0500

    Volver la ruta de descargas universal. Incluir otras opciones de archivos como imagenes, zip y pdf. Mejora de la función create_folder Arreglar el caso donde los archivos tengan el mismo nombre

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ def create_folder(path_downloads, key):
         print("Se ha creado el directorio: %s " % path_downloads)
 
 
-def read_folder(ruta, dic_file):
-    content = os.listdir(ruta)   # lee
-    print(content)
+def read_folder(ruta, dic_file):  # Mejorar esta función
+    content = os.listdir(ruta)  # lee
     for archivo in content:
         if archivo[-4::] == ".exe":
             dic_file['Descargables'].append(archivo)
@@ -22,9 +21,12 @@ def read_folder(ruta, dic_file):
             dic_file['PDF'].append(archivo)
         elif archivo[-4::] == ".zip":
             dic_file['ZIP'].append(archivo)
-        elif archivo[-4::] == ".png" or archivo[-4::] == ".jpg":
+        elif archivo[-4::] == ".png" or archivo[-4::] == ".jpg" or archivo[-4::] == ".gif":
             dic_file['Imagenes'].append(archivo)
-    print(dic_file)
+        elif archivo[-4::] == ".mp4":
+            dic_file["Videos"].append(archivo)
+        elif archivo[-4::] == ".mp3":
+            dic_file["Audio"].append(archivo)
     return dic_file
 
 
@@ -32,14 +34,17 @@ def move_tofolfer(path_downloads, type_file):
     for file in dic_file[type_file]:
         shutil.move(path_downloads + '/' + file, path_downloads + '/' + type_file)
 
+
 if __name__ == '__main__':
-    user_path = os.environ['USERPROFILE'] # Obtiene el camino del usuario
-    path_downloads = user_path + '/Downloads'
-    dic_file = {'Descargables': [], "PDF": [], "ZIP": [], "Imagenes" : []}
-    dic_file = read_folder(path_downloads, dic_file) # Lee los archivos de la carpeta downloads
+    #path_downloads = os.environ['USERPROFILE']  #Obtiene el camino del usuario hacia descargas
+    path_downloads = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
+    # Buscar solucion para computadores donde la carpeta de descargas se encuentra en otro lugar
+
+    dic_file = {'Descargables': [], "PDF": [], "ZIP": [], "Imagenes": [], "Videos": [], "Audio": []}
+    dic_file = read_folder(path_downloads, dic_file)  # Lee los archivos de la carpeta downloads
     for key in dic_file.keys():
         create_folder(path_downloads, key)
     for type_file in dic_file.keys():
         move_tofolfer(path_downloads, type_file)
 
-    print("El proceso ha terminado con exito")
+    #TODO: problemas cuando los archivos tienen el mismo nombre
